4554099a9c321255fdadae3ce6ce2fd58f13ff73
Veloci4 <veloci4raptorsrj453526@gmail.com>
2025-04-27 00:40:13 +0530

    changed author copyright back to original

--- UnitPayloads/F-22A.lua
+++ UnitPayloads/F-22A.lua
@@ -1,6 +1,6 @@
 --[[
     Grinnelli Designs F-22A Raptor
-    Copyright (C) 2025, Ash blythe
+    Copyright (C) 2024, Joseph Grinnelli
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
